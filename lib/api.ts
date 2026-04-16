4f237521f909ef3f07fbc17b2c8ddfceb566a661
asadaslamtech <asadali52pk@gmail.com>
2026-04-16 15:56:05 +0400

    fix env

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://amer-backend.vercel.app";
 
 export async function apiRequest(
   method: string,
